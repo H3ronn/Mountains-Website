0119e7792275fc1707b62400b7210b866b5e067b
Bartosz Czaplicki <h3ronnn@gmail.com>
2022-03-22 14:56:00 +0100

    Add background images animation on scroll

--- build/index.js
+++ build/index.js
@@ -1,8 +1,23 @@
-window.addEventListener('scroll', () => {
+const header = document.querySelector('#header');
+const backgroundImages = [...document.querySelectorAll('.backgroundImage')];
+
+const handleScroll = () => {
   const opacity = (1 - (window.scrollY / 800 )).toFixed(2);
-  const header = document.querySelector('#header');
   header.style.opacity = opacity;
   if (opacity < 0){
     header.style.opacity = 0;
   }
-});
+
+  const translateValue = 0 - (window.scrollY / 23.5);
+  translateBackgroundImages(translateValue)  
+}
+
+const translateBackgroundImages = (value) => {
+  if(value < -30) return
+  backgroundImages[0].style.transform = `translate3d(0px, ${value*2}%, 0px)`;
+  backgroundImages[1].style.transform = `translate3d(0px, ${value}%, 0px)`;
+  backgroundImages[2].style.transform = `translate3d(0px, ${value/3}%, 0px)`;
+}
+
+window.addEventListener('scroll', handleScroll);
+
